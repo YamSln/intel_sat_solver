5799a1e79fd6a9912f9d1544a026f56f2141cdc6
YamSln <yam467@gmail.com>
2025-04-19 14:07:25 +0300

    Added some more optimization test cases

--- topor.optimization.tests/test.cpp
+++ topor.optimization.tests/test.cpp
@@ -30,9 +30,21 @@ protected:
 		}
 		return true;
 	}
+
+	void PrintModel(vector<TToporLitVal> model)
+	{
+		for (int i = 1; i < model.size(); i++)
+		{
+			if (model[i] == TToporLitVal::VAL_SATISFIED)
+				cout << "1 ";
+			else
+				cout << "0 ";
+		}
+		cout << endl;
+	}
 };
 
-double Seminar(std::vector<TToporLitVal> assignment)
+double Seminar(vector<TToporLitVal> assignment)
 {
 	if (assignment[1] == TToporLitVal::VAL_SATISFIED)
 	{
@@ -71,4 +83,90 @@ TEST_F(ToporOptimizationTest, ExampleFromSeminar) {
 	vector<int> expectedModel = { 0, 1, 0 };
 	EXPECT_TRUE(ModelsEqual(model, expectedModel));
 	EXPECT_EQ(value, 8);
+}
+
+double SumSAT(vector<TToporLitVal> assignment)
+{
+	int sum = 0;
+	for (int i = 1; i < assignment.size(); i++)
+	{
+		sum += (assignment[i] == TToporLitVal::VAL_SATISFIED ? 1 : 0);
+	}
+	return sum;
+}
+
+TEST_F(ToporOptimizationTest, SumOfSAT) {
+	CTopor topor;
+	topor.AddClause({ 1, 2, 3 });
+	topor.AddClause({ 2, 4, 9 });
+	topor.AddClause({ 5, 7, 6, 8 });
+
+	auto res = opt.Polosat(&topor, SumSAT);
+	ASSERT_TRUE(res);
+
+	auto [model, value] = *res;
+	EXPECT_EQ(value, 2);
+}
+
+TEST_F(ToporOptimizationTest, SumOfSAT0) {
+	CTopor topor;
+	topor.AddClause({ 1, 2, -3 });
+	topor.AddClause({ 2, -4, 9 });
+	topor.AddClause({ -5, 7, 6, 8 });
+
+	auto res = opt.Polosat(&topor, SumSAT);
+	ASSERT_TRUE(res);
+
+	auto [model, value] = *res;
+	EXPECT_EQ(value, 0);
+}
+
+double WeightedVariables(vector<TToporLitVal> assignment)
+{
+	return
+		(assignment[1] == TToporLitVal::VAL_SATISFIED ? 5 : 0) +
+		(assignment[2] == TToporLitVal::VAL_SATISFIED ? 1 : 0) +
+		(assignment[3] == TToporLitVal::VAL_SATISFIED ? 10 : 0);
+}
+
+TEST_F(ToporOptimizationTest, WeightedVariablePreference) {
+	CTopor topor;
+	topor.AddClause({ 1, 2 });
+	topor.AddClause({ -1, 3 });
+	topor.AddClause({ -2, -3 });
+
+	auto res = opt.Polosat(&topor, WeightedVariables);
+	ASSERT_TRUE(res);
+
+	auto [model, value] = *res;
+
+	EXPECT_EQ(value, 1);
+	vector<int> expectedModel = { 0, 1, 0 };
+	EXPECT_TRUE(ModelsEqual(model, expectedModel));
+}
+
+double XORBehavior(vector<TToporLitVal> assignment)
+{
+	bool a = assignment[1] == TToporLitVal::VAL_SATISFIED;
+	bool b = assignment[2] == TToporLitVal::VAL_SATISFIED;
+	bool out = assignment[3] == TToporLitVal::VAL_SATISFIED;
+
+	return (a ^ b) == out ? 1 : 0;
+}
+
+TEST_F(ToporOptimizationTest, XORLogicRewarded) {
+	CTopor topor;
+	topor.AddClause({ -1, -2, -3 });
+	topor.AddClause({ -1, 2, 3 });
+	topor.AddClause({ 1, -2, 3 });
+	topor.AddClause({ 1, 2, -3 });
+
+	auto res = opt.Polosat(&topor, XORBehavior);
+	ASSERT_TRUE(res);
+
+	auto [model, value] = *res;
+
+	PrintModel(model);
+
+	EXPECT_EQ(value, 1);
 }
